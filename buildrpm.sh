0eca31372571b332100ce53e71d9192d6f88a9b4
yoyoson <yoyoson6@yahoo.com>
2018-03-04 11:01:21 -0500

    Accept a 'packager name' arg and pass to Make to pass to FPM

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -f
+
+if [[ $# -ne 2 ]] ; then
+	echo "	usage: $0 <package name> <packager name (you)>"
+	echo "	e.g.: $0 libevent bobsysadmin"
+	echo "	      $0 tmux bobsysadmin"
+	echo "	      $0 tmux-resurrect bobsysadmin"
+	exit -1
+fi
+
 name=$1
+packager=$2
 mkdir -p fpm-$name
 cp  Makefile.$name  fpm-$name 
 pushd fpm-$name
-make -f ./Makefile.$name
+make -f ./Makefile.$name PACKAGER="${packager}"
 popd
